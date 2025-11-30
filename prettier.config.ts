0bc517ab130a8cb057cb102fef6054d8aece838f
nits68 <nits.laszlo@jedlik.eu>
2025-11-30 19:51:59 +0100

    replace .prettierrc with prettier.config.ts

---
+++ prettier.config.ts
@@ -0,0 +1,11 @@
+import { type Config } from "prettier";
+
+const config: Config = {
+  singleQuote: false,
+  semi: true,
+  trailingComma: "all",
+  tabWidth: 2,
+  printWidth: 100,
+};
+
+export default config;
