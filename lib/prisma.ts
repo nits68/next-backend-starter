b81ba2c50757dda5a9fe7e35ce33714e5913a55d
nits68 <nits.laszlo@jedlik.eu>
2026-02-15 20:51:48 +0100

    correct prismats in lib folder

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,8 @@
 // https://www.prisma.io/docs/orm/more/help-and-troubleshooting/nextjs-help
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/app/prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 const prisma = globalForPrisma.prisma || new PrismaClient();
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
-
