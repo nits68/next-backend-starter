e87ca34117fdc39c9fc6662d21eeef267dd98f77
nits68 <nits.laszlo@jedlik.eu>
2026-02-15 20:40:27 +0100

    remove middleware example

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,49 +1,9 @@
-import { PrismaClient } from "@/app/prisma/client";
+// https://www.prisma.io/docs/orm/more/help-and-troubleshooting/nextjs-help
+import { PrismaClient } from "@prisma/client";
 
-// === Global PrismaClient a Next.js dev hot reload miatt ===
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const prisma = globalForPrisma.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
-// 1. Alap kliens létrehozása (ha még nincs)
-const basePrisma = globalForPrisma.prisma ?? new PrismaClient();
+export default prisma;
 
-// 2. Kliens kiterjesztése a validációhoz ($extends)
-// Fontos: Az $extends új példányt ad vissza, ezt kell exportálni!
-// const prisma = basePrisma.$extends({
-//   query: {
-//     example_model: {
-//       async $allOperations({
-//         args,
-//         query,
-//         operation,
-//       }: {
-//         args: any;
-//         query: any;
-//         operation: string;
-//       }) {
-//         const { field1, field2 } = args.data || {};
-//         if (operation.includes("update") || operation.includes("create")) {
-//           if (field1) {
-//             const builtDate = new Date(field1 as string);
-//             if (builtDate > new Date()) {
-//               throw new Error("field1 conot be in the future!");
-//             }
-//           }
-
-//           if (field2 && (field2 as string).length > 50) {
-//             throw new Error("field2 length must be 50 characters or less!");
-//           }
-//         }
-
-//         return query(args);
-//       },
-//     },
-//   },
-// });
-
-// === Node.js dev hot reload fix ===
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = basePrisma;
-}
-
-// export default prisma; // Ha használunk model $extends-t
-export default basePrisma; // Ha nem használunk $extends-t
