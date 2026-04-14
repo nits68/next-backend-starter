8a5fac619179376a35066f80b7b6201bee468b20
nits68 <nits.laszlo@jedlik.eu>
2026-04-14 09:27:50 +0200

    prisma client new path

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,5 @@
 // https://www.prisma.io/docs/orm/more/help-and-troubleshooting/nextjs-help
-import { PrismaClient } from "@/app/prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 const prisma = globalForPrisma.prisma || new PrismaClient();
